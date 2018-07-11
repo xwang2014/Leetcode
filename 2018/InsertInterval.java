2e0704a1ec8a691172fd47bf698877770cb03709
xwang2014 <x.wang2014@gmail.com>
2018-07-10 22:12:12 -0700

    Improve InsertInterval
    Jul.10, 2018

--- 2018/InsertInterval.java
+++ 2018/InsertInterval.java
@@ -14,48 +14,26 @@ class Solution {
         
         List<Interval> ans = new ArrayList<Interval>();
         
-        int pos = findPosition(intervals, newInterval);
+        int idx = 0;
         
-        intervals.add(pos, newInterval);
-        
-        boolean merged = false;
-        for (int i = 0; i < intervals.size(); i++) {
-            if (i < intervals.size() - 1 && overlap(intervals.get(i), intervals.get(i + 1))) {
-                Interval temp = merge(intervals.get(i), intervals.get(i + 1));
-                intervals.set(i + 1, temp);
-            } else {
-                ans.add(intervals.get(i));
-            }
+        while (idx < intervals.size() && intervals.get(idx).end < newInterval.start) {
+            ans.add(intervals.get(idx));
+            idx += 1;
+        }
+    
+        while (idx < intervals.size() && overlap(intervals.get(idx), newInterval) ) {
+            newInterval = merge(intervals.get(idx), newInterval);
+            idx += 1;
         }
         
+        ans.add(newInterval);
+        
+        while (idx < intervals.size()) {
+            ans.add(intervals.get(idx));
+            idx += 1;
+        }
         
         return ans;
-    }
-    
-    
-    private int findPosition(List<Interval> intervals, Interval in) {
-        if (intervals.size() == 0) return 0;
-        
-        int left = 0, right = intervals.size() - 1;
-        
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            Interval m = intervals.get(mid);
-            
-            if (m.start >= in.start) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        
-        if (intervals.get(left).start >= in.start) {
-            return left;
-        } else if (intervals.get(left).start < in.start && intervals.get(right).start >= in.start) {
-            return right;
-        } else {
-            return right + 1;
-        }
     }
     
     private boolean overlap(Interval in1, Interval in2) {
